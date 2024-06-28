cdd816b1359c737e4edd7250efcadc451d06f45c
Ryan C <rc@ryancash.com>
2024-06-28 10:47:47 -0700

    empty app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div id="windowMain" className="flex flex-col items-center justify-center h-screen w-screen bg-base-100">
+      <div id="appMain" className="bg-base-200 w-3/5 h-3/5 rounded-lg shadow-md">
+    
+      </div>
+    </div>
+  )
+}
+
+export default App
